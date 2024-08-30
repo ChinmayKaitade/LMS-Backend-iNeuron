cc2824575a85fa4eb4b540a313c4d5234a7f3902
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-08-30 16:54:35 +0530

    Database Connection done and MongoDB URL added

---
+++ config/dbConnection.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+mongoose.set("strictQuery", false); // strict mode disabled
+
+const connectionToDB = async () => {
+  try {
+    const { connection } = await mongoose.connect(
+      process.env.MONGO_URI || `mongodb://127.0.0.1:27017/lms`
+    );
+
+    if (connection) {
+      console.log(`Connected to MongoDB: ${connection.host}`);
+    }
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+export default connectionToDB;
