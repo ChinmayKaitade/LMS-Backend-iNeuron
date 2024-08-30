d57def96998a0c5eab4f536769f925c1075ef008
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-08-30 19:56:15 +0530

    User controller logic and model code added

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,17 +1,19 @@
+import AppError from "../utils/error.util";
+
 const register = (req, res) => {
-  //
+  // register route
 };
 
 const login = (req, res) => {
-  //
+  // login route
 };
 
 const logout = (req, res) => {
-  //
+  // logout route
 };
 
 const getProfile = (req, res) => {
-  //
+  // getProfile route
 };
 
 export { register, login, logout, getProfile };
--- models/user.model.js
+++ models/user.model.js
@@ -1,10 +1,53 @@
 import { Schema, model } from "mongoose";
 
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: "String",
+      required: [true, "Full Name is required"],
+      minLength: [5, "Name must be atleast 5 characters"],
+      minLength: [50, "Name should be less than 50 characters"],
+      lowercase: true,
+      trim: true,
+    },
+    email: {
+      type: "String",
+      required: [true, "Email is required"],
+      lowercase: true,
+      trim: true,
+      unique: true,
+      match: [
+        /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+        "Please fill in a valid email address",
+      ],
+    },
+    password: {
+      type: "String",
+      required: [true, "Password is required"],
+      minLength: [8, "Password must be atleast 8 characters"],
+      select: false,
+    },
+    avatar: {
+      public_id: {
+        type: "String",
+      },
+      secure_url: {
+        type: "String",
+      },
+    },
+    role: {
+      type: "String",
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+    forgotPasswordToken: String,
+    forgotPasswordExpiry: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const userSchema = new Schema({
-    
-})
+const User = model("User", userSchema);
 
-const User = model("User", userSchema)
-
-export default User
+export default User;
