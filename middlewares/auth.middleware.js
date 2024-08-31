52c7307d5b42f759fffa455afb7568e9db88bb08
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-08-31 08:36:37 +0530

    Auth Middleware for getProfile route added

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,21 @@
+import AppError from "../utils/error.util";
+import jwt from "jsonwebtoken";
+
+const isLoggedIn = async (req, res, next) => {
+  const { token } = req.cookies;
+
+  // if token doesn't exists
+  if (!token) {
+    return next(new AppError("Unauthenticated, Please Login again", 401));
+  }
+
+  // if token exists --> for getting user details
+  const userDetails = await jwt.verify(token, process.env.JWT_SECRET);
+
+  // user details set into request of user
+  req.user = userDetails;
+
+  next();
+};
+
+export { isLoggedIn };
