68560a79bcf1b709063ddd69888f94ba2257beb5
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-09-01 19:47:53 +0530

    Authentication and Authorization for Payment Route added

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -6,17 +6,44 @@ import {
   getRazorpayApiKey,
   verifySubscription,
 } from "../controllers/payment.controller.js";
+import { authorizedRoles, isLoggedIn } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route("/razorpay-key").get(getRazorpayApiKey);
+router
+    .route("/razorpay-key")
+    .get(
+        isLoggedIn,
+        getRazorpayApiKey
+    );
 
-router.route("/subscribe").post(buySubscription);
+router
+    .route("/subscribe")
+    .post(
+        isLoggedIn,
+        buySubscription
+    );
 
-router.route("/verify").post(verifySubscription);
+router
+    .route("/verify")
+    .post(
+        isLoggedIn,
+        verifySubscription
+    );
 
-router.route("/unsubscribe").post(cancelSubscription);
+router
+    .route("/unsubscribe")
+    .post(
+        isLoggedIn,
+        cancelSubscription
+    );
 
-router.route("/").get(allPayments);
+router
+    .route("/")
+    .get(
+        isLoggedIn,
+        authorizedRoles("ADMIN"),
+        allPayments
+    );
 
 export default router;
